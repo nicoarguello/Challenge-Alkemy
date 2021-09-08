682f2ff444b265ad31b2f21552e8b8fa6043e4fd
nicoarguello <nicoarguello36@gmail.com>
2021-09-08 03:17:59 -0300

    home page

--- Client/src/components/home.js
+++ Client/src/components/home.js
@@ -1,16 +1,38 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { findBudget } from "../redux/actions";
+import Budget from "./budget";
 
-const Home = (props) => {
+const Home = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user)
+     
 
-console.log(props, props.location.search)
+  useEffect((didUpdate) => {
+    dispatch(findBudget(user.id));
+  }, []);
 
-    return (
+//   componentDidMount() {
+//     dispatch(findBudget(user.id));
+//   }
+  const budget = useSelector((store) => store.budget);
+//   console.log(budget, user);
+
+
+
+
+  return (
+    <div>
+      {user.name && user.name.length > 0 ? (
         <div>
-            <p>Home</p>
+          <h1>Welcome {user.name}</h1>
+          <Budget />
         </div>
-    )
-}
+      ) : (
+        <h1>EROR</h1>
+      )}
+    </div>
+  );
+};
 
 export default Home;
-
