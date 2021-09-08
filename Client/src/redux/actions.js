1e484d6c505f055fc7424b54d7632bede45f80b1
nicoarguello <nicoarguello36@gmail.com>
2021-09-08 03:19:42 -0300

    add actions get budget and modify login

--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const GET_USERS = "GET_USERS";
 export const LOGIN = "LOGIN";
 export const FIND_ONE_USER = "FIND_ONE_USER";
+export const FIND_BUDGET = "FIND_BUDGET"
 
 export function addUsers(input) {
   return function () {
@@ -22,17 +23,22 @@ export function findUsers() {
 }
 
 export function addBudget(input) {
-  return function () {
-    axios.post(`http://localhost:3001/addBudget`, input).then((r) => {});
+  return function (dispatch, getState) {
+    const token = getState().token
+    axios.post(`http://localhost:3001/addBudget`, input, {
+      headers: { Authorization: `Bearer ${token}`}}).then((r) => {
+      console.log(r)
+    });
   };
 }
 
 export function login(input) {
   return function (dispatch) {
     axios.post(`http://localhost:3001/login`, input).then((r) => {
+      // console.log(r)
       dispatch({
         type: LOGIN,
-        payload: r,
+        payload: r.data.token,
       });
     });
   };
@@ -45,6 +51,19 @@ export function findOneUser(input) {
       .then((r) => {
         dispatch({
           type: FIND_ONE_USER,
+          payload: r.data,
+        });
+      });
+  };
+}
+
+export function findBudget(id) {
+  return function (dispatch) {
+    axios
+      .get(`http://localhost:3001/findBudget/${id}`)
+      .then((r) => {
+        dispatch({
+          type: FIND_BUDGET,
           payload: r.data,
         });
       });
